2f3cfd8c1e922de088cb4b2f30549ad89c528418
stefano vollono <stefanovollono@gmail.com>
2017-06-17 09:21:50 +0200

    render guides

--- src/guides.js
+++ src/guides.js
@@ -8,12 +8,14 @@
     // contiene la lista di guide create :: potrei usarla per salvarla nel localstorage e poterla riprendere al refresh della pagina
     guides: [
       {
-        type: 'h',
-        top: 123
+        type: 'vollguides__line--h',
+        top: 100,
+        left: 15
       },
       {
-        type: 'v',
-        top: 123
+        type: 'vollguides__line--v',
+        top: 15,
+        left: 100
       }
     ]
 
@@ -24,47 +26,84 @@
     init: function () {
 
       // Delego la view per renderizzare gli elementi del template
-      view.init();
+      structureView.init();
+      guidesWiew.init();
     },
 
     // Mi ritorna tutto l'array di guide
     getGuides: function () {
       return model.guides;
+    },
+
+    // Aggiungo una nuova guida all'array
+    addNewGuide: function (g) {
+      model.guides.push(g);
+
+      // renderizzo le view
+      view.render();
     }
 
   };
 
-  var view = {
+  var structureView = {
 
-    // Salvo il body e invoco il render
     init: function () {
-      this.bodyElem   = document.querySelector('body');
 
-      this.render()
+      this.bodyElem   = document.querySelector('body');
+      this.template = '<div class="vollguides">' +
+                      '<div class="vollguides__overlay"></div>' +
+                      '<div class="vollguides__tooltip"></div>' +
+                      '<div class="vollguides__rule vollguides__rule--h">' +
+                      '<div class="vollguides__rule-pointer"></div>' +
+                      '</div>' +
+                      '<div class="vollguides__rule vollguides__rule--v">' +
+                      '<div class="vollguides__rule-pointer"></div>' +
+                      '</div>' +
+                      '</div>';
+
+      this.render();
+
     },
 
     render: function () {
 
-      var template =  '<div class="vollguides">' +
-                        '<div class="vollguides__overlay"></div>' +
-                        '<div class="vollguides__tooltip"></div>' +
-                        '<div class="vollguides__rule vollguides__rule--h">' +
-                          '<div class="vollguides__rule-pointer"></div>' +
-                        '</div>' +
-                        '<div class="vollguides__rule vollguides__rule--v">' +
-                          '<div class="vollguides__rule-pointer"></div>' +
-                        '</div>' +
-                      '</div>';
+      this.bodyElem.innerHTML = this.template;
+
+    }
+
+  };
 
 
-      // Appendo il template al body del sito
-      this.bodyElem.innerHTML = template;
+  var guidesWiew = {
+
+    init: function () {
+
+      this.guideWrap  = document.querySelector('.vollguides');
+      this.rules = document.querySelectorAll('.vollguides__rule');
+      this.guide = '<div class="vollguides__line"><div class="vollguides__line-inner"></div></div>';
+
+      this.render();
+
+    },
+
+    render: function () {
+
+      // Salvo le guide in una variabile locale
+      var guides = octopus.getGuides();
+
+      // empty the cat list todo devo creare un wrap per le guide e ripulire solo quello
+      //this.guideWrap.innerHTML = '';
+
+      // Renderizzo le guide in base all'array (al primo giro non ce ne sono)
+      for (var i = 0; i < guides.length; i++) {
+        this.guideWrap.insertAdjacentHTML('beforeend', this.guide);
+        var last = $( ".vollguides" ).find('> div').last();
+        last.addClass(guides[i].type).css({top: guides[i].top, left: guides[i].left});
+      }
 
 
-      // Doppio click su una guida invoco octopus per aggiornare l'array delle guide
-      var guideWrap  = document.querySelector('.vollguides');
-      var rules = document.querySelectorAll('.vollguides__rule');
-      var guide = '<div class="vollguides__line"><div class="vollguides__line-inner"></div></div>';
+
+      /*
 
       for (var i = 0 ; i < rules.length; i++) {
         rules[i].addEventListener('click', (function(g) {
@@ -73,22 +112,29 @@
 
             //document.querySelector('.vollguides').appendChild(guide);
             guideWrap.insertAdjacentHTML('beforeend', guide);
+            var last = $( ".vollguides" ).find('> div').last();
 
             // attribuisco la classe in base alla guida cliccata
             if (g.classList.contains('vollguides__rule--h')) {
-              $( ".vollguides" ).find('> div').last().addClass('vollguides__line--h');
-              //document.querySelector('.vollguides').querySelectorAll(".vollguides>div").lastChild.classList.add('vollguides__line--h');
+              last.addClass('vollguides__line--h');
             } else {
-              $( ".vollguides" ).find('> div').last().addClass('vollguides__line--v');
-              //document.querySelector('.vollguides').querySelectorAll(".vollguides>div").lastChild.classList.add('vollguides__line--v');
+              last.addClass('vollguides__line--v');
+            }
+
+            var newGuideObj = {
+              type: 'vollguides__line--h',
+              top: last.offset().top,
+              left: last.offset().left
             }
 
             // aggiorno l'array
-            
+            octopus.addNewGuide(newGuideObj);
+
           }
 
         })(rules[i]));
       }
+      */
 
 
 
