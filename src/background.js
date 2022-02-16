66dfdacc7a0dd68ef0cf7791898f7effbc581fdb
Stefano Vollono <stefanovollono@gmail.com>
2022-02-17 00:48:11 +0100

    migrate to manifest v3

--- src/background.js
+++ src/background.js
@@ -1,9 +1,10 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.tabs.insertCSS(null, {file: "guides.css"});
-  chrome.tabs.executeScript(null, {file: "draggabilly.js"});
-  chrome.tabs.executeScript(null, {file: "guides.js"});
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.insertCSS({
+    target: { tabId: tab.id },
+    files: ['guides.css'],
+  });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['draggabilly.js', 'guides.js'],
+  });
 });
